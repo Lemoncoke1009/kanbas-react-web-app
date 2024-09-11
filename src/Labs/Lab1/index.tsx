68d75aabfb0c89461e2d6a6b8c6166a6acf81601
Lemoncoke1009 <jhh5566727@yahoo.com>
2024-09-11 00:00:37 -0400

    for lab1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -62,9 +62,62 @@ Your favorite books (in no particular order)
   <li>321</li>
   <li>213</li>
 </ul>
-    </div>
-    </div>
-    </div>
+<div id="wd-tables">
+        <h4>Table Tag</h4>
+        <table border={1} width="100%">
+          <thead>
+            <tr>
+              <th>Quiz</th>
+              <th>Topic</th>
+              <th>Date</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Q1</td>
+              <td>HTML</td>
+              <td>2/3/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q2</td>
+              <td>CSS</td>
+              <td>2/10/21</td>
+              <td>90</td>
+            </tr>
+            <tr>
+              <td>Q3</td>
+              <td>JavaScript</td>
+              <td>2/17/21</td>
+              <td>95</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>Average</td>
+              <td>90</td>
+            </tr>
+          </tfoot>
+        </table>
+
+<div id="wd-images">
+  <h4>Image tag</h4>
+  Loading an image from the internet:
+  <br />
+  <img id="wd-starship"
+    width="400px"
+   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+  />
+  <br />
+  Loading a local image:
+  <br />
+  <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+</div>  
+</div>
+</div>
+</div>
+</div>
     );
   }
   

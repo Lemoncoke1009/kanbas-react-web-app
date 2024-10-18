2fa0f8432d8f6799f11f28d7ddb616e9df8c55d4
Lemoncoke1009 <jhh5566727@yahoo.com>
2024-10-17 20:02:07 -0400

    A4

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,33 @@
+import ClickEvent from "./ClickEvent"
+import PassingDataOnEvent from "./PassingDataOnEvent"
+import PassingFunctions from "./PassingFunctions"
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+
 export default function Lab3() {
+  function sayHello() {
+    alert("Hello");
+  }
+
     return (
-      <div id="wd-lab3">
+      
+      <div id="wd-passing-functions">
         <h3>Lab 4</h3>
+        <ClickEvent />
+<PassingDataOnEvent />
+<PassingFunctions theFunction={sayHello} />
+      <EventObject />
+<Counter />
+<BooleanStateVariables />
+<StringStateVariables />
+<DateStateVariable />
+<ObjectStateVariable />
+<ArrayStateVariable />
         </div>
     )
 }

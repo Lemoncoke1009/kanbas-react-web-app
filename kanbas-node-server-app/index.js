d64f2ff1e2eb348e4facc98b50f24cf3ce190f75
Lemoncoke1009 <jhh5566727@yahoo.com>
2024-11-17 15:51:00 -0500

    A5

--- kanbas-node-server-app/index.js
+++ kanbas-node-server-app/index.js
@@ -2,9 +2,15 @@ import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
+
 
 const app = express()
-app.use(cors()); 
+UserRoutes(app);
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}));
 app.use(express.json());
 Lab5(app);
 Hello(app)

de0a1f92073565395b4697fe416329fbffdb9405
Lemoncoke1009 <jhh5566727@yahoo.com>
2024-12-04 16:22:34 -0500

    a6 at

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -50,14 +50,13 @@ export default function Kanbas() {
 };
 
 
-  const [course, setCourse] = useState<any>({
-    _id: "1234",
-    name: "New Course",
-    number: "New Number",
-    startDate: "2023-09-10",
-    endDate: "2023-12-15",
-    description: "New Description",
-  });
+const [course, setCourse] = useState<any>({
+  name: "New Course",
+  number: "New Number",
+  startDate: "2023-09-10",
+  endDate: "2023-12-15",
+  description: "New Description",
+});
 
   const addNewCourse = async () => {
     //const newCourse = await courseClient.createCourse(course);
